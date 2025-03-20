aeeb35fa29964f8c79c6cd812cf4d9e8c3bb9042
Ivanclf <23009201447@stu.xidian.edu.cn>
2025-03-20 15:00:18 +0800

    配置了element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -16,7 +11,6 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
